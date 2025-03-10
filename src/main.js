1c9b78edf077b8a1e75cc77ecbcb4b3628413fda
akaio <Zezinhotwo@users.noreply.github.com.>
2025-03-09 22:13:32 -0300

    Primeiro commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import "./stylesheet/style.css";
+const body = document.querySelector("body");
+
+body.classList.add("hello");
+
+alert("olha");
+
+import printMe from "./print";
+
+const btn = document.createElement('button');
+btn.innerHTML = 'Click me and check the console!';
+btn.onclick = printMe;
+element.appendChild(btn);
+
+// import Icon from './icon.png';
+// Add the image to our existing div.
+//   const myIcon = new Image();
+//   myIcon.src = Icon;
+
+//   element.appendChild(myIcon);
